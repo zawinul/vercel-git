925d3bb273b6be2344fdbb461f933ca061c6addb
Andrenacci, Paolo <paolo.andrenacci@eng.it>
2020-06-28 12:09:50 +0200

    fast deploy

--- api/fattoriale.js
+++ api/fattoriale.js
@@ -1,6 +1,6 @@
 module.exports = (req, res) => {
 	try {
-		res.json({result:req.parameters,saluti:'ciao!'});
+		res.json({result:req.params,saluti:'ciao!'});
 	}
 	catch(e) {
 		res.json({error:""+e});
