ac24adea000d8f0294c5d5d8a8da4b9d378db25b
Andrenacci, Paolo <paolo.andrenacci@eng.it>
2020-06-28 12:15:41 +0200

    fast deploy

--- api/fattoriale.js
+++ api/fattoriale.js
@@ -1,11 +1,15 @@
+const fact = x => x<=1 ? 1 : x*fact(x-1);
+
 module.exports = (req, res) => {
 	try {
+		var x = req.query.x;
+
 		res.json({
 			p:req.params,
 			q:req.query,
 			saluti:'ciao!', 
 			a:28,
-			result:42
+			result:fact(x-0);
 		});
 	}
 	catch(e) {
